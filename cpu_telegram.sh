0f5ece6687e8a4653083d45294b017eaa09d9c1f
Tycoon <tycoon60@rambler.ru>
2022-01-25 16:41:20 +0500

    update

---
+++ cpu_telegram.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Проверяем температуру сервера centos и отправляем в телеграм при привышении.
+#apt-get install lm-sensors
+
+senc=$(sensors | grep "Package id 0:" | awk '{print $4}' | awk '{print int($1+0.5)}')
+
+#echo $senc
+
+if [ ${senc} -gt 25 ] ; then
+	curl -s -X POST https://api.telegram.org/bot1492809911:AAGbke4KMZZw1hUfRX8UCjy2IpB3b5gZItU/sendMessage -F chat_id='-427379370' -F text="Проверить кондиционер"
+fi
