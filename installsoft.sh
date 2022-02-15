070c7594631140b28aba28a25d8e6c5e58081c26
Tycoon60 <72808269+Tycoon60@users.noreply.github.com>
2022-02-15 14:20:23 +0500

    Create installsoft.sh

---
+++ installsoft.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+root_pass="MyPass"
+
+apt update && apt upgrade -y && apt install -y mc ssh chromium-browser gnome-tweaks cups-pdf
+snap install skype --classic
+
+# Включить пользователя root
+echo "${root_pass}\n${root_pass}\n" | passwd root
+
+# Логин root по ssh
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+service ssh restart
+dpkg -i /tmp/nomachine_7.4.1_1_amd64.deb
+
+systemctl stop cups-browsed && systemctl disable cups-browsed
